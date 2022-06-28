0c4bc009595a256e871ada7999abe962ece6056f
Andreaag242 <aaranda96@misena.edu.co>
2022-06-28 11:08:33 -0500

    Actualizacion 2804

---
+++ hospisoftv2/app/controllers/Medico.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+class Medico extends Controller
+{
+  private $MedicoModel;
+  public function __construct()
+   {
+      $this->MedicoModel = $this->loadModel('MedicoModel');
+  }
+   public function index()
+  {
+    $data = $this->MedicoModel->listarMedicos(); //temporal porque no hay dataa
+    // return 'este es el metodo index';
+    $this->getview('Medico',$data);
+  }
+
+  public function generarFormula()
+  {
+    echo 'este es el metodo generar formula del medico ';
+  }
+
+  public function otrometodo()
+  {
+    echo 'este es otro metodo del la controlador medico';
+  }
+}
